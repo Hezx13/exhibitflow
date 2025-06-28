48232ca75322b298df66adea360abc54036b6fe2
Hezx13 <nikolaipozitif@gmail.com>
2025-06-29 01:09:26 +0200

    no rollup

--- vite.config.ts
+++ vite.config.ts
@@ -9,57 +9,5 @@ export default defineConfig({
   },
   build: {
     sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          // Redux ecosystem  
-          'redux-vendor': ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
-          
-          // Material-UI core
-          'mui-core': [
-            '@mui/material', 
-            '@mui/icons-material', 
-            '@emotion/react', 
-            '@emotion/styled',
-            '@mui/styled-engine-sc'
-          ],
-          
-          // Material-UI data components
-          'mui-data': [
-            '@mui/x-data-grid',
-            '@mui/x-data-grid-generator', 
-            '@mui/x-date-pickers',
-            '@mui/x-tree-view',
-            '@mui/x-tree-view-pro'
-          ],
-          
-          // Charts and visualization
-          'charts': ['recharts'],
-          
-          // Grid components
-          'grid': ['ag-grid-community', 'ag-grid-react'],          
-          // Utilities and smaller libraries
-          'utils': [
-            'axios',
-            'dayjs',
-            'js-cookie',
-            'nanoid',
-            'object-hash',
-            'throttle-debounce-ts'
-          ],
-                    
-          // Styling
-          'styling': ['styled-components'],
-          
-          // Other vendor libraries
-          'vendor-misc': [
-            'material-ui-popup-state',
-            'excel-date-to-js',
-            'fractional-indexing-jittered',
-            '@bresatec/smarttrade-frontend-package'
-          ]
-        }
-      }
-    }
   }
 })
